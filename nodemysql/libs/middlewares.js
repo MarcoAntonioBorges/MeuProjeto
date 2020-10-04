74133566114187a97c74c0ed79b8bc0474b4e718
Marco Antônio Borges Oliveira <37504946+MarcoAntonioBorges@users.noreply.github.com>
2020-10-04 14:00:16 -0300

    Update middlewares.js

--- nodemysql/libs/middlewares.js
+++ nodemysql/libs/middlewares.js
@@ -10,17 +10,14 @@ module.exports = app => {
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(bodyParser.json());
 
-    //Configurando Middleware da Aplicação 
+    //Configurando Middleware Global da Aplicação 
     app.use((req, res, next) => {
         //Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexãos
         res.header("Access-Control-Allow-Origin", "*");
         //Quais são os métodos que a conexão pode realizar na API
         res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
         app.use(cors());
-        next();
-    });
-
-    app.use((req, res, next) => {
+        
         // Middleware de pré-execução das rotas
         delete req.body.id;
         next();
