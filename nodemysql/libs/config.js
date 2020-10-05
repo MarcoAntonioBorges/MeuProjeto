723cbbf39449456c1fb11d126d21135b724b16ca
MarcoAntonioBorges <mabo.marcoantonio@gmail.com>
2020-10-05 18:53:16 -0300

    Arrumando o config das variaveis de ambiente .env e renomeando o arquivo .sample-env

--- nodemysql/libs/config.js
+++ nodemysql/libs/config.js
@@ -1,10 +1,12 @@
+const dotenv = require('dotenv').config();
+
 module.exports = {
-    database: process.env.DATABASE,
-    username: process.env.USERNAME,
-    password: process.env.PASSWORD,
+    database: process.env.DATABASE_STORAGE,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
     params: {
-        dialect: process.env.DIALECT,
-        host: process.env.HOST,
-        port: process.env.DB_PORT
+        dialect: process.env.DATABASE_DIALECT,
+        host: process.env.DATABASE_HOST,
+        port: process.env.DATABASE_PORT,
     }
 };
