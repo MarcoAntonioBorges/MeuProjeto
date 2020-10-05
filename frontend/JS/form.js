ce6236a7d23816a32f0c3d6e4381215acada5c10
Daniel <daniel.fecaf@outlook.com>
2020-10-05 19:06:07 -0300

    Atualizando frontend

--- frontend/JS/form.js
+++ frontend/JS/form.js
@@ -4,3 +4,39 @@ if(window.innerWidth <500){
     document.getElementById("titleForm").style.fontSize = "5em";
 }
  
+
+
+var btnSubmit = document.getElementById("btnSubmit");
+
+btnSubmit.addEventListener("click", function(event){
+    event.preventDefault();
+
+    var form = document.getElementById("form");
+
+    const produto = {
+        nm_produto: form.nome.value,
+        ds_produto: form.descricao.value,
+        dt_fabricacao: "2000-05-12",
+        vl_produto: Number(form.preco.value)
+    };
+    
+    console.log('====================================');
+    console.log(produto);
+    console.log('====================================');
+
+    const ENDPOINT_URL = "http://localhost:3000/produtos";
+    const API_HEADERS = {
+        "Content-Type": "application/json"
+    };
+    axios.post(ENDPOINT_URL, JSON.stringify(produto), API_HEADERS)
+        .then((response) => {
+            alert(JSON.stringify(response));
+        })
+        .catch((error) =>{
+            console.error(error);
+        });
+
+        form.reset();
+        form.nome.focus();
+
+});
