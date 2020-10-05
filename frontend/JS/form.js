c5ccdf8ce119684082f25d041ebcf6165c28047c
MarcoAntonioBorges <mabo.marcoantonio@gmail.com>
2020-10-05 19:15:05 -0300

    Atualizando o cadastro da entidade

--- frontend/JS/form.js
+++ frontend/JS/form.js
@@ -1,25 +1,25 @@
-if(window.innerWidth <500){
+if (window.innerWidth < 500) {
     document.getElementById("titleForm").style.fontSize = "2em";
-}else{
+} else {
     document.getElementById("titleForm").style.fontSize = "5em";
 }
- 
+
 
 
 var btnSubmit = document.getElementById("btnSubmit");
 
-btnSubmit.addEventListener("click", function(event){
+btnSubmit.addEventListener("click", function(event) {
     event.preventDefault();
 
     var form = document.getElementById("form");
 
-    const produto = {
+    let produto = {
         nm_produto: form.nome.value,
         ds_produto: form.descricao.value,
-        dt_fabricacao: "2000-05-12",
+        dt_fabricacao: form.dataFabr.value,
         vl_produto: Number(form.preco.value)
     };
-    
+
     console.log('====================================');
     console.log(produto);
     console.log('====================================');
@@ -28,15 +28,15 @@ btnSubmit.addEventListener("click", function(event){
     const API_HEADERS = {
         "Content-Type": "application/json"
     };
-    axios.post(ENDPOINT_URL, JSON.stringify(produto), API_HEADERS)
+    axios.post(ENDPOINT_URL, produto, API_HEADERS)
         .then((response) => {
             alert(JSON.stringify(response));
         })
-        .catch((error) =>{
+        .catch((error) => {
             console.error(error);
         });
 
-        form.reset();
-        form.nome.focus();
+    form.reset();
+    form.nome.focus();
 
 });
